087fa1cb962d93af5660ccc3c8192f7ed54d7fed
Nazmus Sayad <247sayad@gmail.com>
2022-07-07 06:33:27 +0600

    init...

---
+++ index.js
@@ -0,0 +1,28 @@
+const fetch = require("node-fetch")
+const FormData = require("form-data")
+
+const getData = async () => {
+  const res = await fetch("https://sheetdb.io/api/v1/b70w6hs5ccavr")
+  return res.json()
+}
+
+const postData = async (name, msg) => {
+  const form = new FormData()
+  form.append("data[name]", name)
+  form.append("data[msg]", msg)
+
+  return fetch("https://sheetdb.io/api/v1/b70w6hs5ccavr", {
+    method: "POST",
+    body: form,
+  })
+}
+
+const deleteAll = async () => {
+  await fetch("https://sheetdb.io/api/v1/b70w6hs5ccavr/all", {
+    method: "DELETE",
+  })
+
+  console.log("All Data Deleted")
+}
+
+deleteAll()
